21ac38e1d999b41dc3b789a70a5f328b2c258ba0
ysmath0246 <heeju060689@gmail.com>
2025-11-02 00:54:59 +0900

    chore: setup GitHub Pages (Actions) & vite base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 리포지토리 이름과 동일 (https://ysmath0246.github.io/points-only/)
+  base: '/points-only/',
 })
