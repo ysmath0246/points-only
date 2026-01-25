2070dde3d96ee4527a486f46b4304ddb36adcddb
ysmath0246 <heeju060689@gmail.com>
2026-01-25 20:43:46 +0900

    set vite base for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 리포지토리 이름과 동일 (https://ysmath0246.github.io/points-only/)
+  // GitHub Pages 경로 (https://ysmath0246.github.io/points-only/)
   base: '/points-only/',
 })
